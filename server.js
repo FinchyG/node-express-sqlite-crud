655106a1bdd78feb1dc21854cf2ea468911bcaba
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-05-23 08:03:01 +0100

    addition of cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Create express app
-var express = require("express")
-var app = express()
-var db = require("./database.js")
-var md5 = require("md5")
+var express = require("express");
+var app = express();
+var cors = require('cors');
+var db = require("./database.js");
+var md5 = require("md5");
 
+app.use(cors());
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
